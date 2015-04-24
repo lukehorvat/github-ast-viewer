405f9474e37aac0f376cebfb1a71552c83a10275
Luke Horvat <lukehorvat@gmail.com>
2015-04-25 02:35:46 +1000

    Use esprima to get AST from JS file

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import "babelify/polyfill";
 import $ from "jquery";
+import esprima from "esprima";
 
 $(() => {
   let file = $(".final-path").text();
 
   if (file.endsWith(".js")) {
     let rawButton = $("#raw-url");
-    let astButton = rawButton.clone()
+    let astButton = rawButton
+      .clone()
       .text("AST")
-      .attr("id", "ast-url")
       .attr("href", "#")
+      .removeAttr("id")
       .insertBefore(rawButton)
       .click(() => {
         $.get(rawButton.attr("href"))
         .done(data => {
-          console.log("success", data);
+          let ast = esprima.parse(data);
+          console.log(ast);
         })
         .fail((jqXHR, textStatus) => {
           console.log("error", jqXHR, textStatus);
