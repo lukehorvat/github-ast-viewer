82a195a2b8e40d2f967a238b73743050b49d2cfc
Luke Horvat <lukehorvat@gmail.com>
2015-04-25 02:28:32 +1000

    Inject AST button into the page

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+import "babelify/polyfill";
 import $ from "jquery";
 
-console.log("!", $);
+$(() => {
+  let file = $(".final-path").text();
+
+  if (file.endsWith(".js")) {
+    let rawButton = $("#raw-url");
+    let astButton = rawButton.clone()
+      .text("AST")
+      .attr("id", "ast-url")
+      .attr("href", "#")
+      .insertBefore(rawButton)
+      .click(() => {
+        $.get(rawButton.attr("href"))
+        .done(data => {
+          console.log("success", data);
+        })
+        .fail((jqXHR, textStatus) => {
+          console.log("error", jqXHR, textStatus);
+        });
+      });
+  }
+});
